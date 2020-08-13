7f824e1fdddaa227cdd284142f401c2512047510
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2020-08-13 15:59:12 +0200

    Simplify code removing over-verbose

--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 """Cucumber Basket feature tests."""
-
+from functools import partial
 from pytest_bdd import (
     given,
     parsers,
@@ -16,39 +16,24 @@ scenarios("../features/cucumbers.feature")
 
 EXTRA_TYPES = {"Number": int}
 
+parse_num = partial(parsers.cfparse, extra_types=EXTRA_TYPES)
 
-@given(
-    parsers.cfparse(
-        'the basket has "{initial:Number}" cucumbers', extra_types=EXTRA_TYPES
-    )
-)
+
+@given(parse_num('the basket has "{initial:Number}" cucumbers'))
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
 
-@when(
-    parsers.cfparse(
-        '"{some:Number}" cucumbers are added to the basket', extra_types=EXTRA_TYPES,
-    )
-)
+@when(parse_num('"{some:Number}" cucumbers are added to the basket'))
 def add_cucumbers(basket, some):
     basket.add(some)
 
 
-@when(
-    parsers.cfparse(
-        '"{some:Number}" cucumbers are removed from the basket',
-        extra_types=EXTRA_TYPES,
-    )
-)
+@when(parse_num('"{some:Number}" cucumbers are removed from the basket'))
 def remove_cucumbers(basket, some):
     basket.remove(some)
 
 
-@then(
-    parsers.cfparse(
-        'the basket contains "{total:Number}" cucumbers', extra_types=EXTRA_TYPES
-    )
-)
+@then(parse_num('the basket contains "{total:Number}" cucumbers'))
 def basket_has_total(basket, total):
     assert basket.count == total
